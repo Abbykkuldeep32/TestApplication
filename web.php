d46974cac8e33096a63e4319f7401edc1d6dcc39
Abbykkuldeep32 <163218475+Abbykkuldeep32@users.noreply.github.com>
2024-03-13 18:39:32 +0530

    Update web.php

--- web.php
+++ web.php
@@ -1,2 +1,3 @@
 Route::post('/your-endpoint', 'YourController@yourMethod');
 Route::patch('/tasks/{id}/complete', 'TaskController@complete');
+Route::get('/tasks', 'TaskController2@index')->name('tasks.index');
