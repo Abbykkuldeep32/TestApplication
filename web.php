2bc99f9bba6e14b13a816de869efb0d9012c29a9
Abbykkuldeep32 <163218475+Abbykkuldeep32@users.noreply.github.com>
2024-03-13 18:43:50 +0530

    Update web.php

--- web.php
+++ web.php
@@ -1,3 +1,5 @@
 Route::post('/your-endpoint', 'YourController@yourMethod');
 Route::patch('/tasks/{id}/complete', 'TaskController@complete');
 Route::get('/tasks', 'TaskController2@index')->name('tasks.index');
+Route::delete('/tasks/{task}', 'TaskController3@delete')->name('tasks.delete');
+
